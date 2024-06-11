f5c2821acf6c3a518ddd848624af837eb51b6608
evaldo <evaaldormj@gmail.com>
2024-06-11 17:15:18 -0300

    Feat: Adicionados bindings aos product

--- src/app/Components/product/product.component.ts
+++ src/app/Components/product/product.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-
+  @Input() titulo: string = "Título do Produto";
+  @Input() preco: string = "0.00";
+  @Input() urlImagem: string = "../../../assets/imagens/coffee-icon-vector.jpg";
 }
