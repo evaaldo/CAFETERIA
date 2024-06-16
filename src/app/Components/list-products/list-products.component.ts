0e2109cb66a7199e51261d8ec6c72d4b1e2552cd
evaldo <evaaldormj@gmail.com>
2024-06-16 10:34:13 -0300

    Feat: tentativa de filtro por texto

--- src/app/Components/list-products/list-products.component.ts
+++ src/app/Components/list-products/list-products.component.ts
@@ -3,6 +3,16 @@ import { ContainerComponent } from '../container/container.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ProductComponent } from './product/product.component';
 import { ContainerProductsComponent } from './container-products/container-products.component';
+import { FormsModule } from '@angular/forms';
+
+interface Product {
+  id: number,
+  titulo: string,
+  preco: string,
+  urlImagem: string
+}
+
+import products from './products.json'
 
 @Component({
   selector: 'app-list-products',
@@ -11,11 +21,25 @@ import { ContainerProductsComponent } from './container-products/container-produ
     ContainerComponent,
     WelcomeComponent,
     ProductComponent,
-    ContainerProductsComponent
+    ContainerProductsComponent,
+    FormsModule
   ],
   templateUrl: './list-products.component.html',
   styleUrl: './list-products.component.css'
 })
-export class ListProductsComponent {
 
+export class ListProductsComponent {
+  productsList: Product[] = products;
+  filterByText: string = "";
+  filteredProducts: Product[] = this.productsList;
+
+  filterProductsByText(): void {
+    if (!this.filterByText) {
+      this.filteredProducts = this.productsList;
+    } else {
+      this.filteredProducts = this.productsList.filter(product =>
+        product.titulo.toLowerCase().includes(this.filterByText.toLowerCase())
+      );
+    }
+  }
 }
--- src/app/Components/list-products/welcome/welcome.component.ts
+++ src/app/Components/list-products/welcome/welcome.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
